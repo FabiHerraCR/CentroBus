5967383d7a069a630bf3303c242afbe058fdd854
FabiHerraCR <fabian.munoz.herra@est.una.ac.cr>
2026-05-10 00:39:09 -0600

    FINAL TESTS

--- database/primer_laravel.sql
+++ database/primer_laravel.sql
@@ -1,6 +1,10 @@
+CREATE DATABASE primer_laravel;
+
 USE primer_laravel;
 
 SHOW TABLES;
+
+SELECT * FROM clientes;
 
 SELECT * FROM rutas;
 
@@ -24,18 +28,32 @@ FROM tiquetes t
 INNER JOIN clientes c ON t.cliente_id = c.id
 INNER JOIN rutas r ON t.ruta_id = r.id;
 
--- Ver datos de registros de clientes
-SELECT
+
+SELECT 
+    t.id,
+    t.codigo,
+    c.nombre,
+    c.apellidos,
+    r.origen,
+    r.destino,
+    r.horario,
+    t.fecha_viaje,
+    t.precio
+FROM tiquetes t
+INNER JOIN clientes c ON t.cliente_id = c.id
+INNER JOIN rutas r ON t.ruta_id = r.id
+ORDER BY t.id DESC;
+
+-- ver datos tarjetas
+SELECT 
     id,
     nombre,
     apellidos,
     pasaporte,
-    nacionalidad,
     correo,
     telefono,
     tarjeta,
     ccv,
-    fecha_vencimiento,
-    password AS contrasena,
-    created_at AS fecha_registro
+    fecha_vencimiento
 FROM clientes;
+
